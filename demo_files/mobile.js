3340e210dbc8e89b265b61c3bc33d39140a1c827
Tejaswini-kb <tejaswini.sharma@timeinc.com>
2016-09-26 16:42:22 +0530

    Update mobile.js

--- demo_files/mobile.js
+++ demo_files/mobile.js
@@ -1286,11 +1286,12 @@ var d = document.getElementById("block-ti-lsg-mr-ads-mobile-320x320");
 
 console.log(d);
     if(elementInViewport(d)) {
-       d.classList.add('zoomoutad');
+       d.classList.add('zoomad');
+      d.classList.remove('zoomoutad');
+      
+    } else {
+      d.classList.add('zoomoutad');
      d.classList.remove('zoomad');
      
-    } else {
-         d.classList.add('zoomad');
-      d.classList.remove('zoomoutad');
     }
  };
